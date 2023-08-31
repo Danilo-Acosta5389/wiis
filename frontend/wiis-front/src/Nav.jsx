efeb0d87c7c0e57cc89336b4c6baa38731930637
Danilo Acosta <danilo.acostadote@chasacademy.se>
2023-08-31 02:10:46 +0200

    Added hamburger btn

--- frontend/wiis-front/src/Nav.jsx
+++ frontend/wiis-front/src/Nav.jsx
@@ -5,30 +5,23 @@ const NavContainer = styled.div`
 display: flex;
 flex-direction: row;
 align-items: center;
-justify-content: end;
+justify-content: center;
 /* border: 3px solid red; */
-height: fit-content;
+height: 5em;
 width: 100%;
 z-index: 2;
 position: fixed;
-
-
-//Hamburger meny goes here
-`;
-const Button = styled.p`
-border: 2px solid gray;
-border-radius: 4px;
-padding: 10px;
-font-size: 2em;
-color: gray;
-margin-top: 1rem;
-margin-right: 1rem;
 `;
 
 function Nav() {
     return(
         <NavContainer>
-            <Button>X</Button>
+            <input className="checkbox" type="checkbox"/>
+            <div className="hamburger-lines">
+              <span className="line line1"></span>
+              <span className="line line2"></span>
+              <span className="line line3"></span>
+            </div>  
         </NavContainer>
     )
 }
