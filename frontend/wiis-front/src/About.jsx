07f6dedb8f8e57db838eb683275ca736f1992120
Danilo Acosta <danilo.acostadote@chasacademy.se>
2023-09-06 23:31:59 +0200

    Made headers clickable

--- frontend/wiis-front/src/About.jsx
+++ frontend/wiis-front/src/About.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import styled from 'styled-components';
 
 
@@ -10,18 +10,50 @@ width: 100vw;
 `;
 
 
-const aboutHeader = "About";
+const AboutHeader = styled.h1`
+cursor: pointer;
+&.transform {
+  color: blue;
+}
+`;
+
 const aboutInfo = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vitae consequat sapien. Integer tempor urna eu commodo consequat. Maecenas aliquet vestibulum ex, in tristique arcu accumsan ut. Sed eleifend scelerisque ullamcorper. Quisque lobortis metus turpis. Praesent vulputate nibh ut est varius consectetur. Nam tristique arcu id pharetra posuere. Aenean sodales semper metus, ut ornare diam pharetra non.";
 
 
+
+
 function About() {
+  useEffect(() => {
+    const aboutSelect = document.querySelector('.aboutHeader');
+    
+    let clicked = false;
+    const handleClick = () => {
+      if (clicked) {
+        aboutSelect.classList.remove('transform');
+      } else {
+        console.log('clicked');
+        aboutSelect.classList.add('transform');
+      }
+      clicked = !clicked;
+    };
+
+    aboutSelect.addEventListener('click', handleClick);
+    // clicked = true;
+    return () => {
+      aboutSelect.removeEventListener('click', handleClick);
+    };
+  }, []);
 
     return(
     <AboutContainer>
-      <h1>{aboutHeader}</h1>
+      <AboutHeader className="aboutHeader">About</AboutHeader>
       {/* <p>{aboutInfo}</p> */}
     </AboutContainer>
     );
-}
+};
+
+
+
+
 
 export default About;
