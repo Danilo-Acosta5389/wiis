6a0b68d6f1cbd5601825eb34a8bd94c4660f590b
Danilo Acosta <danilo.acostadote@chasacademy.se>
2023-09-20 17:02:57 +0200

    Header animations and text

--- frontend/wiis-front/src/About.jsx
+++ frontend/wiis-front/src/About.jsx
@@ -1,38 +1,75 @@
 import React, {useEffect} from "react";
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 
 const AboutContainer = styled.section`
-display: flex;
-flex-direction: column;
-height: 100vh;
-width: 100vw;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 100vw;
+  & h1 {
+    position: relative;
+  }
 `;
 
 
 const AboutHeader = styled.h1`
-cursor: pointer;
-&.transform {
-  color: blue;
+  cursor: pointer;
+  transition-duration: 1000ms;
+  transition-property: transform;
+  transition-timing-function: ease-in-out;
+  z-index: 1;
+  
+  &.transform {
+    transform: translateY(-6em);
+
 }
 `;
 
-const aboutInfo = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vitae consequat sapien. Integer tempor urna eu commodo consequat. Maecenas aliquet vestibulum ex, in tristique arcu accumsan ut. Sed eleifend scelerisque ullamcorper. Quisque lobortis metus turpis. Praesent vulputate nibh ut est varius consectetur. Nam tristique arcu id pharetra posuere. Aenean sodales semper metus, ut ornare diam pharetra non.";
 
+
+const aboutText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vitae consequat sapien. Integer tempor urna eu commodo consequat. Maecenas aliquet vestibulum ex, in tristique arcu accumsan ut. Sed eleifend scelerisque ullamcorper. Quisque lobortis metus turpis. Praesent vulputate nibh ut est varius consectetur. Nam tristique arcu id pharetra posuere. Aenean sodales semper metus, ut ornare diam pharetra non.";
+
+
+const AboutInfo = styled.p`
+  display: flex;
+  width: 80vw;
+  height: fit-content;
+  text-align: center;
+  flex-wrap: wrap;
+  position: absolute;
+  z-index: 0;
+  opacity: 0;
+  transition-duration: 300ms;
+  transition-property: opacity;
+  transition-timing-function: linear;
+
+  &.show {
+    opacity: 1;
+    transition-delay: 600ms;
+    transition-duration: 500ms;
+    
+
+  }
+
+`;
 
 
 
 function About() {
   useEffect(() => {
     const aboutSelect = document.querySelector('.aboutHeader');
+    const aboutTextShow = document.querySelector('.aboutText');
     
     let clicked = false;
     const handleClick = () => {
       if (clicked) {
         aboutSelect.classList.remove('transform');
+        aboutTextShow.classList.remove('show');
       } else {
         console.log('clicked');
         aboutSelect.classList.add('transform');
+        aboutTextShow.classList.add('show');
       }
       clicked = !clicked;
     };
@@ -47,7 +84,7 @@ function About() {
     return(
     <AboutContainer>
       <AboutHeader className="aboutHeader">About</AboutHeader>
-      {/* <p>{aboutInfo}</p> */}
+      <AboutInfo className="aboutText">{aboutText}</AboutInfo>
     </AboutContainer>
     );
 };
--- frontend/wiis-front/src/Contact.jsx
+++ frontend/wiis-front/src/Contact.jsx
@@ -2,29 +2,71 @@ import React, {useEffect} from 'react';
 import styled from 'styled-components';
 
 const ContactContainer = styled.section`
-height: 100vh;
-width: 100vw;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 100vw;
+  & h1 {
+    position: relative;
+  }
 `;
 
 const ContactHeader = styled.h1`
-cursor: pointer;
-&.transform {
-  color: blue;
+  cursor: pointer;
+  transition-duration: 800ms;
+  transition-property: transform;
+  transition-timing-function: ease-in-out;
+  z-index: 1;
+  
+  &.transform {
+    transform: translateY(-4em);
+
 }
 `;
+
+const ContactInfo = styled.p`
+  display: flex;
+  width: 80vw;
+  height: fit-content;
+  text-align: center;
+  flex-wrap: wrap;
+  position: absolute;
+  z-index: 0;
+  opacity: 0;
+  transition-duration: 300ms;
+  transition-property: opacity;
+  transition-timing-function: linear;
+
+  &.show {
+    opacity: 1;
+    transition-delay: 500ms;
+    transition-duration: 500ms;
+
+  }
+
+`;
+
+const contactText = `Maecenas vitae consequat sapien. Integer tempor urna eu commodo consequat. 
+
+Maecenas aliquet vestibulum ex, in tristique arcu accumsan ut. Sed eleifend scelerisque ullamcorper. 
+
+Quisque lobortis metus turpis. Praesent vulputate nibh ut est varius consectetur.`;
 
 
 function Contact() {
   useEffect(() => {
     const contactSelect = document.querySelector('.contactHeader');
+    const contactTextShow = document.querySelector('.contactText');
     
     let clicked = false;
     const handleClick = () => {
       if (clicked) {
         contactSelect.classList.remove('transform');
+        contactTextShow.classList.remove('show');
       } else {
         console.log('clicked');
         contactSelect.classList.add('transform');
+        contactTextShow.classList.add('show');
       }
       clicked = !clicked;
     };
@@ -39,6 +81,7 @@ function Contact() {
   return(
       <ContactContainer>
       <ContactHeader className='contactHeader'>Contact</ContactHeader>
+      <ContactInfo className='contactText'>{contactText}</ContactInfo>
       </ContactContainer>
   );
 };
