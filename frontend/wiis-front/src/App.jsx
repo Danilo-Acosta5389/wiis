a9e4b72ee5a2e9c6c7c4552fa34026acc663f256
Danilo Acosta <danilo.acostadote@chasacademy.se>
2023-10-09 18:31:26 +0200

    removed video

--- frontend/wiis-front/src/App.jsx
+++ frontend/wiis-front/src/App.jsx
@@ -27,9 +27,9 @@ function App() {
 
   return (
     <>
-      <VideoBg className="video" muted={true} loop={true} autoPlay>
+      {/* <VideoBg className="video" muted={true} loop={true} autoPlay>
         <source src="https://player.vimeo.com/external/314181352.hd.mp4?s=03e7a8ddba6566f8a62cd7f8a8da7d79461c3dbd&profile_id=172&oauth2_token_id=57447761" type="video/mp4"/>Your web-browser does not support video background
-      </VideoBg>
+      </VideoBg> */}
       <Nav />
       <Title />
       <About />
