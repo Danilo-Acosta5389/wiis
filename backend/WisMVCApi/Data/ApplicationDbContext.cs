178bdfa0b7c21f49f9a58ea2e58b13f6ff6cd7a0
Danilo Acosta <danilo.acostadote@chasacademy.se>
2023-10-10 02:19:45 +0200

    Added a new webApi project

---
+++ backend/WisMVCApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WisMVCApi.Models;
+
+#nullable disable
+
+namespace WisMVCApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+        }
+
+        public DbSet<QuestionsModel> Questions { get; set; }
+        public DbSet<AnswersModel> Answers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Mapping Foreign Keys
+            // AnswersTable
+            modelBuilder.Entity<AnswersModel>()
+                .HasOne(x => x.Questions)
+                .WithMany(x => x.Answers)
+                .HasForeignKey(x => x.QuestionId);
+        }
+    }
+}
---
+++ backend/WisMVCApi/Models/AnswersModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+#nullable disable
+
+namespace WisMVCApi.Models
+{
+    public class AnswersModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        [Required]
+        public int Number { get; set; }
+
+        [Required]
+        public string IpAddress { get; set; }
+
+        [Required]
+        public int QuestionId { get; set; }
+
+        public virtual QuestionsModel Questions { get; set; }
+    }
+}
